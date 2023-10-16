5a4333b9bfb7256d80d90ec4bcb6c13fe6b58364
Daniel Heilbron <134653973+st10082120@users.noreply.github.com>
2023-10-16 22:56:03 +0200

    fixed login and register

--- app/src/main/java/com/example/opsc_birdwatch/AccountManager.kt
+++ app/src/main/java/com/example/opsc_birdwatch/AccountManager.kt
@@ -1,13 +1,13 @@
 package com.example.opsc_birdwatch
 
-object UserManager {
-    private val users: HashMap<String, String> = HashMap()
+object AccountManager {
+    private val Account: HashMap<String, String> = HashMap()
 
     fun addUser(username: String, password: String) {
-        users[username] = password
+        Account[username] = password
     }
 
     fun getUserPassword(username: String): String? {
-        return users[username]
+        return Account[username]
     }
 }
