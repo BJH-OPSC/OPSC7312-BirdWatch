a1ed7c592bd0f072e7fe389c9f45bb1fe29bb0e2
mattbosch <63168928+mattbosch@users.noreply.github.com>
2023-10-19 11:58:39 +0200

    Comments on RetrofitInterface.kt

--- app/src/main/java/com/example/opsc_birdwatch/Geolocation/RetrofitInterface.kt
+++ app/src/main/java/com/example/opsc_birdwatch/Geolocation/RetrofitInterface.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// Interface using Retrofit to define the GET request for retrieving a list of hotspots.
 interface RetrofitInterface {
     @GET("ref/hotspot/geo")
     fun getHotspots(
