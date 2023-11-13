e254ec778da088288edc3e9185f3546b4cdaff88
mattbosch <mattbosch16@gmail.com>
2023-11-13 16:43:25 +0200

    MapActivity.kt added firestore references.

--- app/src/main/java/com/example/opsc_birdwatch/MapActivity.kt
+++ app/src/main/java/com/example/opsc_birdwatch/MapActivity.kt
@@ -9,7 +9,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.content.res.Resources
-import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
@@ -45,6 +44,7 @@ import androidx.constraintlayout.helper.widget.MotionEffect
 import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, OnPolylineClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
     //declaring class variables
@@ -70,6 +70,10 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWin
     private var lastHotspotLoadLocation: Location? = null
     private val hotspotReloadDistanceThreshold = 10000
     private var currentLocationCallback: LocationCallback? = null
+
+    // Reference to a Firestore collection
+    private val db = FirebaseFirestore.getInstance()
+    private val collectionName = "BirdObservations"
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
