01f1f3b3e3f5d1b6fd5bef39c0bdb220adbfaea8
Daniel J <danjacobs2306@gmail.com>
2023-11-13 16:01:12 +0200

    bruh

--- app/src/main/java/com/example/opsc_birdwatch/HelperClass.kt
+++ app/src/main/java/com/example/opsc_birdwatch/HelperClass.kt
@@ -60,4 +60,94 @@ class HelperClass {
             }
     }*/
 
+
+    data class Achievement(
+        val id: String,
+        val name: String,
+        val description: String,
+        val conditions: List<Condition>
+    )
+
+    data class Condition(
+        val type: ConditionType,
+        val target: String,
+        val value: Any // Use Any to handle both Int and Double
+    )
+
+    enum class ConditionType {
+        DISTANCE_TRAVELED,
+        BIRDS_ADDED,
+        MARKER_PLACED
+    }
+
+    object AchievementManager {
+        private val unlockedAchievements = mutableListOf<Achievement>()
+
+        // Define a list of achievements
+        private val achievementList = listOf(
+            Achievement(
+                id = "distance_5km",
+                name = "Novice Traveler",
+                description = "Travel 5 kilometers",
+                conditions = listOf(Condition(ConditionType.DISTANCE_TRAVELED, "", 5))
+            ),
+            Achievement(
+                id = "birds_5",
+                name = "Bird Spotter",
+                description = "Add 5 birds to the observation list",
+                conditions = listOf(Condition(ConditionType.BIRDS_ADDED, "", 5))
+            ),
+            Achievement(
+                id = "marker_placed",
+                name = "Navigator",
+                description = "Place your first marker on the map",
+                conditions = listOf(Condition(ConditionType.MARKER_PLACED, "", 1))
+            )
+            // Add more achievements as needed
+        )
+
+        fun trackDistanceTraveled(distance: Number) {
+            checkAndUnlockAchievements(ConditionType.DISTANCE_TRAVELED, distance)
+        }
+
+        fun trackBirdsAdded(count: Int) {
+            checkAndUnlockAchievements(ConditionType.BIRDS_ADDED, count)
+        }
+
+        fun trackMarkerPlaced() {
+            checkAndUnlockAchievements(ConditionType.MARKER_PLACED, 1)
+        }
+
+        private fun checkAndUnlockAchievements(type: ConditionType, value: Number) {
+            val relevantAchievements = achievementList.filter { it.conditions.any { it.type == type } }
+
+            relevantAchievements.forEach { achievement ->
+                achievement.conditions.filter { it.type == type }.forEach { condition ->
+                    val conditionValue = when (condition.value) {
+                        is Int -> (condition.value as Int).toDouble()
+                        is Double -> condition.value as Double
+                        else -> throw IllegalArgumentException("Unsupported condition value type")
+                    }
+
+                    val userValue = value.toDouble()
+
+                    if (userValue >= conditionValue) {
+                        unlockAchievement(achievement)
+                    }
+                }
+            }
+        }
+
+        private fun unlockAchievement(achievement: Achievement) {
+            if (!unlockedAchievements.contains(achievement)) {
+                unlockedAchievements.add(achievement)
+                // You can notify the user, update UI, or store the achievement.
+            }
+        }
+
+        fun getUnlockedAchievements(): List<Achievement> {
+            return unlockedAchievements.toList()
+        }
+    }
+
 }
